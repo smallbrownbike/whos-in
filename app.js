d978e957566f084cf91b2d492d68a38278f7884c
smallbrownbike <snscrn@gmail.com>
2017-08-04 12:07:04 -0700

    implement /api/members route

--- app.js
+++ app.js
@@ -4,15 +4,63 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
       Scraper = require ('images-scraper'),
-      google = new Scraper.Google();
+      NB = require('nodebrainz'),
+      nb = new NB({userAgent:'my-awesome-app/0.0.1 ( http://my-awesome-app.com )'}),
+      bing = new Scraper.Bing();
+
+
+
+
+
 
 app.use(cors())
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/api/image', (req, res) => {
-  console.log(req.body)
+app.post('/api/members', (req, res) => {
+  var group = req.body.group;
+  nb.search('artist', {artist: group}, function(err, response){
+    nb.artist(response.artists[0].id, {inc:'artist-rels'}, function(err, response){
+      var currentMembers = []
+      var pastMembers = []
+      var members = []
+      var count = 0;
+      response.relations.forEach((rel) => {
+        var name = rel.artist.name;
+        bing.list({
+          keyword: name + ' ' + group,
+          num: 1
+        })
+        .then((image) => {
+          if(rel.ended){
+            pastMembers.push({
+              name: name,
+              begin: rel.begin,
+              end: rel.end,
+              instrument: rel.attributes,
+              image: image[0].url ? image[0].url : null
+            })
+          } else {
+            currentMembers.push({
+              name: name,
+              begin: rel.begin,
+              end: rel.end,
+              instrument: rel.attributes,
+              image: image[0].url ? image[0].url : null
+            })
+          }
+          count += 1;
+          if(count === response.relations.length){
+            members.push(currentMembers, pastMembers)
+            res.json(members)
+          }
+        }).catch((err) => {
+            console.log('err',err);
+        })
+      })
+    })
+  });
 })
 
 app.listen(process.env.PORT || 8181, process.env.IP, function(){
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import wiki from 'wikijs';
-import CircularProgress from 'material-ui/CircularProgress';
-import Paper from 'material-ui/Paper';
-import {GridList, GridTile} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import Subheader from 'material-ui/Subheader';
+import { Card, Image } from 'semantic-ui-react'
+
+class Cards extends React.Component{
+  render(){
+    return(
+      <Card.Group itemsPerRow={3}>
+        {this.props.members.map((member) => {
+          return (
+            <Card
+              onClick={() => {console.log('click')}}
+            >
+              <Card.Content>
+                <Image avatar={true} floated='right' size='large' src={member.image} />
+                <Card.Header>
+                  {member.name}
+                </Card.Header>
+                <Card.Meta>
+                  {this.props.current ? member.begin ? member.begin + ' to Now' : '' : member.begin && member.end ? member.begin + ' to ' + member.end : ''}
+                </Card.Meta>
+                <Card.Description>
+                  {member.instrument.join(', ')}
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          )
+        })}
+      </Card.Group>
+    )
+  }
+}
 
 class Main extends React.Component {
   constructor(props){
@@ -12,46 +37,21 @@ class Main extends React.Component {
     this.state = {loading: true, group: decodeURIComponent(window.location.pathname.slice(1))}
   }
   componentWillMount(){
-    wiki().page(this.state.group)
-    .then(page => page.info('currentMembers'))
-    .then(info => {
-      console.log(info)
-      if(info){
-        var images = {}
-        info.forEach((member, index) => {
-          fetch('http://localhost:8181/api/image', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({member: member})})
-          .then(response => response.json)
-          .then(json => {
-            images[member] = json;
-            if(index === info.length - 1){
-              this.setState({images: images, current: info, loading: false})
-            }
-          })
-        })
-      } else {
-        this.setState({loading: false})
-      }
+    fetch('http://localhost:8181/api/members', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({group: this.state.group})})
+    .then(response => response.json())
+    .then(json => {
+      this.setState({current: json[0], past: json[1], loading: false})
+      console.log(this.state)
     })
   }
   render() {
     if(!this.state.loading && this.state.current){
       return (
-        <div className='center'>
-          
-            <GridList>
-              {this.state.current.map((member, index) => {
-                return(
-                  <GridTile
-                    key={index}
-                    title={member}
-                  >
-                    <img src={'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'} />
-                  </GridTile>
-                )
-              })}
-            </GridList>
-          
-          
+        <div className='ui container'>
+          <h1>Likely Current Members</h1>
+            <Cards members={this.state.current} current={true}/>
+          <h1>Likely Past Members</h1>
+            <Cards members={this.state.past} current={false}/>
         </div>
       );
     } else if(!this.state.loading && !this.state.current) {
@@ -61,7 +61,7 @@ class Main extends React.Component {
     } else {
       return (
         <div className='center'>
-          <CircularProgress color={'black'} size={80} thickness={5} />
+          
         </div>
       )
     }
