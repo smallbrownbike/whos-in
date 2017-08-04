3ec00f0ae2f04a280c682b3229b8406b5d91190c
smallbrownbike <snscrn@gmail.com>
2017-08-03 19:30:10 -0700

    show current members

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import wiki from 'wikijs';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {loading: true, group: decodeURIComponent(window.location.pathname.slice(1))}
+  }
+  componentWillMount(){
+    wiki().page(this.state.group)
+    .then(page => page.info('currentMembers'))
+    .then(info => {
+      if(info){
+        this.setState({current: info})
+      }
+      this.setState({loading: false})
+    })
+  }
   render() {
-    return (
-      <div className="App">
-        hellooooooo
-      </div>
-    );
+    if(!this.state.loading && this.state.current){
+      return (
+        <div>
+          {this.state.current.map((member, index) => {
+            return(
+              <li key={index}>
+                {member}
+              </li>
+            )
+          })}
+        </div>
+      );
+    } else if(!this.state.loading && !this.state.current) {
+      return (
+        <div>Couldn't find any active band members in <span className='red'>{this.state.group}</span></div>
+      )
+    } else {
+      return (
+        <div>Loading....</div>
+      )
+    }
   }
 }
 
