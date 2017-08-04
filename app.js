e082c9b2b807af19139bbfa618b4e19508855684
smallbrownbike <snscrn@gmail.com>
2017-08-04 12:41:05 -0700

    add person api route

--- app.js
+++ app.js
@@ -21,44 +21,76 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/api/members', (req, res) => {
   var group = req.body.group;
   nb.search('artist', {artist: group}, function(err, response){
-    nb.artist(response.artists[0].id, {inc:'artist-rels'}, function(err, response){
-      var currentMembers = []
-      var pastMembers = []
-      var members = []
-      var count = 0;
-      response.relations.forEach((rel) => {
-        var name = rel.artist.name;
-        bing.list({
-          keyword: name + ' ' + group,
-          num: 1
-        })
-        .then((image) => {
-          if(rel.ended){
-            pastMembers.push({
-              name: name,
-              begin: rel.begin,
-              end: rel.end,
-              instrument: rel.attributes,
-              image: image[0].url ? image[0].url : null
-            })
-          } else {
-            currentMembers.push({
-              name: name,
-              begin: rel.begin,
-              end: rel.end,
-              instrument: rel.attributes,
-              image: image[0].url ? image[0].url : null
+    var mbid = response.artists[0].id;
+    nb.artist(mbid, {inc:'artist-rels'}, function(err, response){
+      if(response.type === 'Person'){
+          nb.artist(mbid, {inc:'url-rels'}, function(err, response){
+          if(response.relations){
+            var links = []
+            response.relations.forEach((link) => {
+              if(link.type === 'bandcamp' || link.type === 'social network' || link.type === 'wikipedia'){
+                links.push(link.url.resource)
+              }
             })
           }
-          count += 1;
-          if(count === response.relations.length){
-            members.push(currentMembers, pastMembers)
-            res.json(members)
-          }
-        }).catch((err) => {
-            console.log('err',err);
+          bing.list({
+            keyword: group,
+            num: 1
+          })
+          .then((image) => {
+            res.json([
+              {
+                person: true,
+                name: response.name,
+                image: image[0].url,
+                born: response['life-span'].begin,
+                country: response.country,
+                area: response.begin_area.name,
+                gender: response.gender,
+                links: links
+              }
+            ])
+          })
         })
-      })
+      } else {
+        var currentMembers = []
+        var pastMembers = []
+        var members = []
+        var count = 0;
+        response.relations.forEach((rel) => {
+          var name = rel.artist.name;
+          bing.list({
+            keyword: name + ' ' + group,
+            num: 1
+          })
+          .then((image) => {
+            if(rel.ended){
+              pastMembers.push({
+                name: name,
+                begin: rel.begin,
+                end: rel.end,
+                instrument: rel.attributes,
+                image: image[0].url ? image[0].url : null
+              })
+            } else {
+              currentMembers.push({
+                name: name,
+                begin: rel.begin,
+                end: rel.end,
+                instrument: rel.attributes,
+                image: image[0].url ? image[0].url : null
+              })
+            }
+            count += 1;
+            if(count === response.relations.length){
+              members.push(currentMembers, pastMembers)
+              res.json(members)
+            }
+          }).catch((err) => {
+              console.log('err',err);
+          })
+        })
+      }
     })
   });
 })
