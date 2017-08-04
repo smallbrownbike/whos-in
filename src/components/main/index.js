70f0135b318be7ce15c154a3969ef0331f6c2525
smallbrownbike <snscrn@gmail.com>
2017-08-03 20:42:02 -0700

    add material ui

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import wiki from 'wikijs';
+import CircularProgress from 'material-ui/CircularProgress';
+import Paper from 'material-ui/Paper';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
 
-class App extends React.Component {
+const style = {
+  width: 600,
+  height: 600
+}
+
+class Main extends React.Component {
   constructor(props){
     super(props)
     this.state = {loading: true, group: decodeURIComponent(window.location.pathname.slice(1))}
@@ -19,26 +29,36 @@ class App extends React.Component {
   render() {
     if(!this.state.loading && this.state.current){
       return (
-        <div>
-          {this.state.current.map((member, index) => {
-            return(
-              <li key={index}>
-                {member}
-              </li>
-            )
-          })}
+        <div className='center'>
+          
+            <GridList>
+              {this.state.current.map((member, index) => {
+                return(
+                  <GridTile
+                    key={index}
+                    title={member}
+                  >
+                    <img src={'https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'} />
+                  </GridTile>
+                )
+              })}
+            </GridList>
+          
+          
         </div>
       );
     } else if(!this.state.loading && !this.state.current) {
       return (
-        <div>Couldn't find any active band members in <span className='red'>{this.state.group}</span></div>
+        <div className='center'>Couldn't find any active band members in <span className='red'>{this.state.group}</span></div>
       )
     } else {
       return (
-        <div>Loading....</div>
+        <div className='center'>
+          <CircularProgress color={'black'} size={80} thickness={5} />
+        </div>
       )
     }
   }
 }
 
-export default App;
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
-import App from './components/main';
+import Main from './components/main';
+
+const App = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Main />
+  </MuiThemeProvider>
+);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
