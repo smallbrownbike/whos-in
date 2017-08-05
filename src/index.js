9f8af5b4cf77066dc53838998ee3a29e448e1781
smallbrownbike <snscrn@gmail.com>
2017-08-05 08:16:17 -0700

    add react router/remove mui

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import './index.css';
 import Main from './components/main';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './index.css';
 
-const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <Main />
-  </MuiThemeProvider>
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path = '/' component={Main} />
+      <Route path = '*' component={Main} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
